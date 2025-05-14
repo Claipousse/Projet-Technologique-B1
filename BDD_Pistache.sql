c297b21327479a288767a134aac5934180d3c4cd
Clément <clementconrie@gmail.com>
2025-05-14 20:11:02 +0200

    Fix script MySQL

--- BDD_Pistache.sql
+++ BDD_Pistache.sql
@@ -1,89 +1,89 @@
-create database bdd_pistache;
-use bdd_pistache;
+CREATE DATABASE bdd_pistache;
+USE bdd_pistache;
 
-create table utilisateur(
-    id_utilisateur int not null auto_increment,
-    nom varchar(50) not null,
-    prenom varchar(50) not null,
-    email varchar(100) not null unique,
-    mot_de_passe varchar(150) not null,
-    role ENUM('admin','participant') not null, --ENUM est un type de donnée qui permet de limiter la valeur d’un champ à une liste précise de choix.
-    constraint PK_UTI primary key (id_utilisateur)
+CREATE TABLE genre(
+    id_genre INT NOT NULL AUTO_INCREMENT,
+    nom_genre VARCHAR(50) NOT NULL,
+    CONSTRAINT PK_GENRE PRIMARY KEY (id_genre)
 );
 
-create table jeux(
-    id_jeux int not null auto_increment,
-    nom varchar(50) not null unique,
-    description_courte text not null,
-    description_longue text not null,
-    annee_sortie year not null,
-    date_ajout date not null,
-    id_genre int not null,
-    id_type int not null,
-    constraint PK_JEUX primary key (id_jeux),
-    constraint FK_GENRE foreign key (id_genre) references genre (id_genre),
-    constraint FK_TYPE foreign key (id_type) references type (id_type)
+CREATE TABLE type(
+    id_type INT NOT NULL AUTO_INCREMENT,
+    nom_type VARCHAR(50) NOT NULL,
+    CONSTRAINT PK_TYPE PRIMARY KEY (id_type)
 );
 
-create table genre(
-    id_genre int not null auto_increment,
-    nom_genre varchar(50) not null,
-    constraint PK_GENRE primary key (id_genre)
+CREATE TABLE utilisateur(
+    id_utilisateur INT NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    mot_de_passe VARCHAR(150) NOT NULL,
+    role ENUM('admin','participant') NOT NULL, /* ENUM est un type de donnée qui permet de limiter la valeur d'un champ à une liste précise de choix. */
+    CONSTRAINT PK_UTI PRIMARY KEY (id_utilisateur)
 );
 
-create table type(
-    id_type int not null auto_increment,
-    nom_type varchar(50) not null,
-    constraint PK_TYPE primary key (id_type)
+CREATE TABLE jeux(
+    id_jeux INT NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(50) NOT NULL UNIQUE,
+    description_courte TEXT NOT NULL,
+    description_longue TEXT NOT NULL,
+    annee_sortie YEAR NOT NULL,
+    date_ajout DATE NOT NULL,
+    id_genre INT NOT NULL,
+    id_type INT NOT NULL,
+    CONSTRAINT PK_JEUX PRIMARY KEY (id_jeux),
+    CONSTRAINT FK_GENRE FOREIGN KEY (id_genre) REFERENCES genre (id_genre),
+    CONSTRAINT FK_TYPE FOREIGN KEY (id_type) REFERENCES type (id_type)
 );
 
-create table ressource(
-    id_ressource int not null auto_increment,
-    id_jeux int not null,
-    type_ressource varchar(100) not null, --"image" ou "vidéo" etc...
-    url text not null,
-    titre text not null,
-    constraint PK_RESS primary key (id_ressource),
-    constraint FK_JEUX foreign key (id_jeux) references jeux (id_jeux)
+CREATE TABLE ressource(
+    id_ressource INT NOT NULL AUTO_INCREMENT,
+    id_jeux INT NOT NULL,
+    type_ressource VARCHAR(100) NOT NULL, /* "image" ou "vidéo" etc... */
+    url TEXT NOT NULL,
+    titre TEXT NOT NULL,
+    CONSTRAINT PK_RESS PRIMARY KEY (id_ressource),
+    CONSTRAINT FK_JEUX_RESS FOREIGN KEY (id_jeux) REFERENCES jeux (id_jeux)
 );
 
-create table evenement(
-    id_evenement int not null auto_increment,
-    titre varchar(100) not null,
-    description text not null, 
-    date_debut date not null,
-    date_fin date not null,
-    capacite_max int not null,
-    duree_type varchar(50)not null,
-    constraint PK_event primary key (id_evenement)
+CREATE TABLE evenement(
+    id_evenement INT NOT NULL AUTO_INCREMENT,
+    titre VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL, 
+    date_debut DATE NOT NULL,
+    date_fin DATE NOT NULL,
+    capacite_max INT NOT NULL,
+    duree_type VARCHAR(50) NOT NULL,
+    CONSTRAINT PK_EVENT PRIMARY KEY (id_evenement)
 );
 
-create table jeux_evenement(
-    id_jeux_event int not null auto_increment,
-    id_jeux int not null,
-    id_evenement int not null,
-    constraint PK_JEUX_EVENT primary key (id_jeux_event),
-    constraint FK_JEUX foreign key (id_jeux) references jeux (id_jeux),
-    constraint FK_EVENT foreign key (id_evenement) references evenement (id_evenement)
+CREATE TABLE jeux_evenement(
+    id_jeux_event INT NOT NULL AUTO_INCREMENT,
+    id_jeux INT NOT NULL,
+    id_evenement INT NOT NULL,
+    CONSTRAINT PK_JEUX_EVENT PRIMARY KEY (id_jeux_event),
+    CONSTRAINT FK_JEUX_EVENT FOREIGN KEY (id_jeux) REFERENCES jeux (id_jeux),
+    CONSTRAINT FK_EVENT_JEUX FOREIGN KEY (id_evenement) REFERENCES evenement (id_evenement)
 );
 
-create table inscription(
-    id_inscription int not null auto_increment,
-    id_utilisateur int not null,
-    id_evenement int not null,
-    nb_accompagnant int, 
-    date_inscription date not null,
-    status ENUM('en attente', 'validé', 'annulé'),
-    constraint PK_INSC primary key (id_inscription),
-    constraint FK_UTI foreign key (id_utilisateur) references utilisateur (id_utilisateur),
-    constraint FK_EVENT foreign key (id_evenement) references evenement (id_evenement)
+CREATE TABLE inscription(
+    id_inscription INT NOT NULL AUTO_INCREMENT,
+    id_utilisateur INT NOT NULL,
+    id_evenement INT NOT NULL,
+    nb_accompagnant INT, 
+    date_inscription DATE NOT NULL,
+    status ENUM('en attente', 'validé', 'annulé') NOT NULL,
+    CONSTRAINT PK_INSC PRIMARY KEY (id_inscription),
+    CONSTRAINT FK_UTI_INSC FOREIGN KEY (id_utilisateur) REFERENCES utilisateur (id_utilisateur),
+    CONSTRAINT FK_EVENT_INSC FOREIGN KEY (id_evenement) REFERENCES evenement (id_evenement)
 );
 
-create table preferences(
-    id_preference int not null auto_increment,
-    id_inscription int not null,
-    id_jeux int not null,
-    constraint PK_PREF primary key (id_preference),
-    constraint FK_INSC foreign key (id_inscription) references inscription (id_inscription),
-    constraint FK_JEUX foreign key (id_jeux) references jeux (id_jeux)
+CREATE TABLE preferences(
+    id_preference INT NOT NULL AUTO_INCREMENT,
+    id_inscription INT NOT NULL,
+    id_jeux INT NOT NULL,
+    CONSTRAINT PK_PREF PRIMARY KEY (id_preference),
+    CONSTRAINT FK_INSC_PREF FOREIGN KEY (id_inscription) REFERENCES inscription (id_inscription),
+    CONSTRAINT FK_JEUX_PREF FOREIGN KEY (id_jeux) REFERENCES jeux (id_jeux)
 );
