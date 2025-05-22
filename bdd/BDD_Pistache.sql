644171d8146cc59570037320a3c862aa12044c0a
lydia-guett <lydiaguett11@gmail.com>
2025-05-22 19:44:01 +0200

    maj table jeux bdd

--- bdd/BDD_Pistache.sql
+++ bdd/BDD_Pistache.sql
@@ -125,3 +125,20 @@ VALUES
     ("Soirée Créative Dixit", "Laisse parler ton imagination et ton intuition avec le jeu Dixit ! Une soirée pleine de poésie, d’illustrations magnifiques et de devinettes subtiles. Parfait pour se détendre et rire ensemble.", "2025-06-28", "2025-06-28", 10, "soirée"),
     ("Tournoi Uno + Mille Bornes", "Affronte les autres participants dans un tournoi dynamique où Uno et Mille Bornes seront à l’honneur. De la stratégie, du fun, et surtout beaucoup de cartes à jouer !", "2025-07-06", "2025-07-06", 20, "journée"),
     ("Week-end Mystères & Déductions", "Résous des enquêtes, bluffe tes amis et déduis l’impossible dans un week-end spécial Cluedo, Codenames et Scrabble ! Idéal pour les esprits curieux et logiques.", "2025-07-13", "2025-07-14", 10, "week-end");
+
+-- insertion jeux
+INSERT INTO jeux (nom, description_courte, description_longue, annee_sortie, date_ajout, id_genre, id_type, image_path)
+VALUES 
+    ('Les Colons de Catane', 'Jeu de stratégie et de développement primé', 'Développez des colonies, construisez des routes et des villes, et échangez des ressources dans ce jeu de stratégie et de développement primé.', 1995, CURDATE(), 6, 4, 'assets/images/jeux/ColonsDeCatane.png'),
+    ('Dixit', 'Jeu d’imagination et d’intuition', 'Un jeu d’imagination et d’intuition où vous devez deviner ou faire deviner des cartes à partir d’indices subtils et créatifs.', 2008, CURDATE(), 1, 6, 'assets/images/jeux/dixit.png'),
+    ('Pandemic', 'Jeu coopératif captivant', 'Travaillez en équipe pour enrayer la propagation de quatre maladies mortelles et sauver l’humanité dans ce jeu coopératif captivant.', 2008, CURDATE(), 8, 3, 'assets/images/jeux/pandemic.png'),
+    ('7 Wonders', 'Jeu de cartes et de développement rapide', 'Développez votre civilisation et construisez une merveille qui traversera les âges dans ce jeu de cartes et de développement rapide.', 2010, CURDATE(), 3, 2, 'assets/images/jeux/7wonders.png'),
+    ('Uno', 'Le jeu de cartes familial par excellence', 'Le jeu de cartes familial par excellence où il faut être le premier à se débarrasser de toutes ses cartes en les assortissant par couleur ou par numéro.', 1971, CURDATE(), 8, 2, 'assets/images/jeux/uno.png'),
+    ('Cluedo', 'Jeu d’enquête classique', 'Menez l’enquête pour découvrir qui a commis le crime, avec quelle arme et dans quelle pièce dans ce jeu d’enquête classique.', 1949, CURDATE(), 8, 9, 'assets/images/jeux/cluedo.png'),
+    ('Exploding Kittens', 'Jeu de cartes stratégique rempli d’humour', 'Un jeu de cartes stratégique rempli d’humour où vous devez éviter de piocher les chatons explosifs pour rester dans la partie.', 2015, CURDATE(), 8, 2, 'assets/images/jeux/ExplodingKittens.png'),
+    ('Azul', 'Jeu de placement tactique aux règles simples mais profondes', 'Devenez artisan et décorez le palais royal avec de magnifiques carreaux de céramique dans ce jeu de placement tactique aux règles simples mais profondes.', 2017, CURDATE(), 3, 10, 'assets/images/jeux/azul.png'),
+    ('Codenames', 'Jeu d’association d’idées simple mais plein de subtilités', 'Un jeu d’association d’idées où vous devez faire deviner des mots à votre équipe en utilisant un seul indice. Simple à comprendre mais plein de subtilités!', 2015, CURDATE(), 8, 9, 'assets/images/jeux/CodeNames.png'),
+    ('Takenoko', 'Jeu aux mécaniques innovantes et à l’esthétique charmante', 'Cultivez le bambou, nourrissez le panda et aménagez le jardin de l’empereur du Japon dans ce jeu aux mécaniques innovantes et à l’esthétique charmante.', 2011, CURDATE(), 3, 4, 'assets/images/jeux/takenoko.png'),
+    ('Mille Bornes', 'Le célèbre jeu de cartes de course automobile', 'Le célèbre jeu de cartes de course automobile où il faut parcourir 1000 km tout en évitant les embûches tendues par vos adversaires.', 1954, CURDATE(), 8, 2, 'assets/images/jeux/MilleBornes.png'),
+    ('Scrabble', 'Le jeu de lettres classique', 'Le jeu de lettres classique où vous devez former des mots entrecroisés sur un plateau pour marquer un maximum de points.', 1948, CURDATE(), 8, 6, 'assets/images/jeux/scrabble.png');
+    
